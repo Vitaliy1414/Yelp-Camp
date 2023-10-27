aac43ddaa4048850cc8f126fe77106c0e043c627
Vitaliy1414 <zvarych.v1998@gmail.com>
2023-10-27 12:33:04 +0300

    Initial commit

---
+++ routes/campgrounds.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const campgrounds = require('../controllers/campgrounds');
+const catchAsync = require('../utils/catchAsync');
+const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
+const multer = require('multer');
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
+
+const Campground = require('../models/campground');
+
+router.route('/')
+    .get(catchAsync(campgrounds.index))
+    .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground))
+  
+
+
+router.get('/new', isLoggedIn, campgrounds.renderNewForm)
+
+
+router.get('/:id', catchAsync(campgrounds.showCampgrounds));
+
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEdit));
+
+router.put('/:id', isLoggedIn, isAuthor, upload.array('image'), validateCampground, catchAsync(campgrounds.updateCampground));
+
+router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
+
+module.exports = router;
